b3bf50006e3aba7d98b18dc842434106bcfaf11c
gabriellejousse <gabrielle.jousse@gmail.com>
2020-10-29 16:55:29 +0100

    étape 4 finie

--- express_basics/index.js
+++ express_basics/index.js
@@ -4,22 +4,24 @@ const app = express();
 const port = 3001;
 
 
-const arrAuthors = ['Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US', 'Oscar Wilde, UK']
-const arrBooks = ['Beowulf', 'Hamlet, Othello, Romeo and Juliet, MacBeth',
-'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest']
+const arrAuthors = ['Lawrence Nowell', 'William Shakespeare', 'Charles Dickens', 'Oscar Wilde']
+const arrBooks = ['Beowulf', 'Hamlet, Othello, Romeo and Juliet MacBeth',
+    'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest']
+const arrCountries = ['UK', 'UK', 'US', 'UK']
+
 
 // ex 0 :
 
 app.get('/', (req, res) => {
     res.send('Authors API');
-  });
+});
 
 // ex 1 : 
 
 // utiliser les params dynamiques (:num etc) pour éviter de faire un app.get pour chaque auteur 
 
-app.get('/authors/:numAuthors/', (req, res) => {
-    res.send(arrAuthors[req.params.numAuthors - 1])
+app.get('/authors/:id/', (req, res) => {
+    res.send(arrAuthors[req.params.id - 1])
 
 })
 
@@ -27,16 +29,89 @@ app.get('/authors/:numAuthors/', (req, res) => {
 
 // utiliser les params dynamiques (:num etc) pour éviter de faire un app.get pour chaque book
 
-app.get('/authors/:numBooks/books/', (req, res) => {
-    res.send(arrBooks[req.params.numBooks - 1])
+app.get('/authors/:id/books/', (req, res) => {
+    res.send(arrBooks[req.params.id - 1])
 })
+
+
+//ex 4 :
+
+const authorsObj = [
+    {
+        name: 'Lawrence Nowell',
+        country: 'UK'
+    },
+    {
+        name: 'William Shakespeare',
+        country: 'UK'
+    },
+    {
+        name: 'Charles Dickens',
+        country: 'US'
+    },
+    {
+        name: 'Oscar Wilde',
+        country: 'UK'
+    }
+]
+
+const booksObj = [
+    {
+        book: ['Beowulf']
+    },
+    {
+        book: ['Hamlet, Othello, Romeo and Juliet, MacBeth']
+    },
+    {
+        book: ['Oliver Twist, A Christmas Carol']
+    }, 
+    {
+        book: ['The Picture of Dorian Gray, The Importance of Being Earnest']
+    }
+] 
+
+app.get('/json/authors/:id/books/', (req, res) => {
+
+    const newBooks = {
+        books: booksObj[req.params.id -1].book
+    }
+    console.log("NewBooks", newBooks)
+    res.json(newBooks)
+})
+
+
+//
+app.get('/json/authors/:id/', (req, res) => {
+    
+    const newAuthors = {
+        name: authorsObj[req.params.id -1].name,
+        country: authorsObj[req.params.id -1].country
+    }
+    console.log("newAuthors", newAuthors)
+    res.json(newAuthors)
+
+})
+
+
+
+
+//faire .json 
 
 
 // ex 3 :
 
 // gestion d'erreur sur les pages d'auteurs :
-app.get('/authors/*', (req, res) => {
-    res.send(`The author with the ID _ does not exist`);
+
+// app.get('/authors/:*/', (req, res) => {
+//     res.send(`The author with the ID ${req.params.*} does not exist`);
+// });
+
+app.get('/authors/:id/', function (req, res, next) {
+    //si l'id de l'url est > 4, renvoyer un message d'erreur avec le nom de l'id problématique:
+    if (req.params.id > 4) {
+        res.send(`error ID ${req.params.id}`)
+        console.log(`coucou error ${req.params.id}`);
+    }
 });
 
 //gestion d'erreur sur ttes les pages :
@@ -44,9 +119,8 @@ app.get('*', (req, res) => {
     res.send('error');
 });
 
-//ex 4 :
 
-
+// partie "instructions" :
 app.listen(port, () => {
     console.log('Server started on port:' + port);
 })
